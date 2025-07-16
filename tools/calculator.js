9a4a49a42387bd24012065eb3a9c30c7b91c2b5c
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-07-16 17:24:56 +0100

    added logic to highlight calculator keys when pressed

--- tools/calculator.js
+++ tools/calculator.js
@@ -59,62 +59,136 @@ backButton.addEventListener("click", () => {
 
 document.addEventListener("keydown", (e) => {
     if (e.key === "1") {
-        console.log(1)
       screenArray.push(1);
-      screenText.innerText = screenArray.join('');  
+      screenText.innerText = screenArray.join('');
+      oneButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "2") {
       screenArray.push(2);
-      screenText.innerText = screenArray.join('');  
+      screenText.innerText = screenArray.join(''); 
+      twoButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "3") {
         screenArray.push(3);
         screenText.innerText = screenArray.join('');  
+        threeButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "4") {
         screenArray.push(4);
         screenText.innerText = screenArray.join('');  
+        fourButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "5") {
         screenArray.push(5);
         screenText.innerText = screenArray.join('');  
+        fiveButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "6") {
         screenArray.push(6);
         screenText.innerText = screenArray.join('');  
+        sixButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "7") {
         screenArray.push(7);
-        screenText.innerText = screenArray.join('');  
+        screenText.innerText = screenArray.join(''); 
+        sevenButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "8") {
         screenArray.push(8);
         screenText.innerText = screenArray.join('');  
+        eightButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "9") {
         screenArray.push(9);
         screenText.innerText = screenArray.join('');  
+        nineButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "0") {
         screenArray.push(0);
         screenText.innerText = screenArray.join('');  
+        zeroButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "Backspace") {
         screenArray.pop()
         screenText.innerText = screenArray.join('')
+        backButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "+") {
         screenArray.push("+");
         screenText.innerText = screenArray.join('')
+        addButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "=") {
         screenText.innerText = eval(screenText.innerText);
         screenArray = [];
         screenArray.push(screenText.innerText)
+        equalsButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "Enter") {
         screenText.innerText = eval(screenText.innerText);
         screenArray = [];
-        screenArray.push(screenText.innerText)
+        screenArray.push(screenText.innerText);
+        equalsButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "*") {
         screenArray.push("*");
         screenText.innerText = screenArray.join('');  
+        multiplyButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "/") {
         screenArray.push("/");
         screenText.innerText = screenArray.join('');  
+        divideButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === "-") {
         screenArray.push("-");
         screenText.innerText = screenArray.join('');  
+        subtractButton.style.backgroundColor = `var(--button-color-hover)`
     } else if (e.key === ".") {
         screenArray.push(".");
         screenText.innerText = screenArray.join('');  
+        decimalButton.style.backgroundColor = `var(--button-color-hover)`
     }
-    
+})
+
+document.addEventListener("keyup", (e) => {
+    if (e.key === "1") {
+
+      oneButton.style.backgroundColor = ``
+    } else if (e.key === "2") {
+
+      twoButton.style.backgroundColor = ``
+    } else if (e.key === "3") {
+ 
+        threeButton.style.backgroundColor = ``
+    } else if (e.key === "4") {
+
+        fourButton.style.backgroundColor = ``
+    } else if (e.key === "5") {
+
+        fiveButton.style.backgroundColor = ``
+    } else if (e.key === "6") {
+ 
+        sixButton.style.backgroundColor = ``
+    } else if (e.key === "7") {
+
+        sevenButton.style.backgroundColor = ``
+    } else if (e.key === "8") {
+
+        eightButton.style.backgroundColor = ``
+    } else if (e.key === "9") {
+
+        nineButton.style.backgroundColor = ``
+    } else if (e.key === "0") {
+
+        zeroButton.style.backgroundColor = ``
+    } else if (e.key === "Backspace") {
+
+        backButton.style.backgroundColor = ``
+    } else if (e.key === "+") {
+
+        addButton.style.backgroundColor = ``
+    } else if (e.key === "=") {
+ 
+        equalsButton.style.backgroundColor = ``
+    } else if (e.key === "Enter") {
+
+        equalsButton.style.backgroundColor = ``
+    } else if (e.key === "*") {
+
+        multiplyButton.style.backgroundColor = ``
+    } else if (e.key === "/") {
+
+        divideButton.style.backgroundColor = ``
+    } else if (e.key === "-") {
+
+        subtractButton.style.backgroundColor = ``
+    } else if (e.key === ".") {
+
+        decimalButton.style.backgroundColor = ``
+    }
 })
