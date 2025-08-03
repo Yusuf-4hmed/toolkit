1dfaf7ae9e040817aee9b973e0cce99c4d1939be
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-08-03 10:33:18 +0100

    added in temp convert styling layout and logic from previous project

--- tools/calculator.js
+++ tools/calculator.js
@@ -295,3 +295,33 @@ const enforceNumberOnly = (e) => {
 
 priceInput.addEventListener('input', enforceNumberOnly)
 customTip.addEventListener('input', enforceNumberOnly)
+
+// temp convert
+
+const tempInput = document.getElementById('temp-input')
+const tempOutupt = document.getElementById('temp-output')
+
+const tempInputRegex = /^\-?\d*\.?\d*$/
+
+const celciusToFarenheit = () => {
+    tempOutupt.value = Math.round((tempInput.value * 9/5) + 32)
+}
+const farenheitToCelcius = () => {
+    tempInput.value = Math.round((tempOutupt.value - 32) * 5/9)
+}
+
+tempInput.addEventListener('input', (e) => {
+    if (!tempInputRegex.test(tempInput.value)) {
+        e.target.value = e.target.value.slice(0, -1)
+    } else {
+     celciusToFarenheit()   
+    }
+    
+})
+tempOutupt.addEventListener('input', (e) => {
+    if (!tempInputRegex.test(tempOutupt.value)) {
+        e.target.value = e.target.value.slice(0, -1)
+    } else {
+     farenheitToCelcius()   
+    }
+})
