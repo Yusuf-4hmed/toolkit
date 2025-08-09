bb6fb2487ab2e269bd660f62d866d5c0a460d752
Yusuf Ahmed <yusufahmed0625@gmail.com>
2025-08-09 13:49:15 +0100

    added logic for length conversion and clear button

--- tools/convert.js
+++ tools/convert.js
@@ -1,6 +1,9 @@
 const conversionInput = document.getElementById('conversion-input');
 const conversionInputSelect = document.getElementById('conversion-input-select');
 
+const conversionOutput = document.getElementById('conversion-output');
+const conversionOutputSelect = document.getElementById('conversion-output-select');
+
 const convertInput = () => {
     let meters;
 
@@ -23,9 +26,45 @@ const convertInput = () => {
         break;
     }
 
-    console.log(meters)
+    let output;
+
+        switch (conversionOutputSelect.value) {
+        case "mm": output = meters / 0.001;
+        break;
+        case "cm": output = meters / 0.01;
+        break;
+        case "m": output = meters / 1;
+        break;
+        case "km": output = meters / 1000;
+        break;
+        case "in": output = meters / 0.0254;
+        break;
+        case "ft": output = meters / 0.3048;
+        break;
+        case "yd": output = meters / 0.9144;
+        break;
+        case "mi": output = meters / 1609.344;
+        break;
+    }
+    
+    conversionOutput.value = output;
 }
 
 conversionInput.addEventListener('input', () => {
    convertInput()
 })
+conversionInputSelect.addEventListener('change', () => {
+    convertInput()
+})
+conversionOutputSelect.addEventListener('change', () => {
+    convertInput()
+})
+
+const conversionClearButton = document.getElementById('conversion-clear-button');
+
+const clearAll = () => {
+    conversionInput.value = '';
+    conversionOutput.value = '';
+}
+
+conversionClearButton.addEventListener('click', clearAll)
